9e632b3d91cecaba95d48819020f81680592e4c9
randima217 <152683475+randima-edussuriya@users.noreply.github.com>
2025-07-02 10:06:43 +0530

    Dynamic footer year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <footer className='py-2 border-top border-4 mt-5'>
-            &copy; 2025 GPA Calculator | Developed by <a href='https://github.com/randima-edussuriya' target='_blank' className='text-reset'>Randima Edussuriya</a>
+            &copy; {new Date().getFullYear()} GPA Calculator | Developed by <a href='https://github.com/randima-edussuriya' target='_blank' className='text-reset'>Randima Edussuriya</a>
         </footer>
     )
 }
