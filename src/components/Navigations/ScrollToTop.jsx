b1dca7872bdb1a65b094cfb1aaa728ede97df7a2
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-08-12 12:18:24 +0530

    feat: add scrollToTop functinality

--- src/components/Navigations/ScrollToTop.jsx
+++ src/components/Navigations/ScrollToTop.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { Button, Container } from 'react-bootstrap'
 
 function ScrollToTop() {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+    }
+
     return (
         <Container fluid className='fixed-bottom text-end'>
             <Button
+                onClick={scrollToTop}
                 variant='light'
-                // size='lg'
                 className='btnScrolToTop shadow mb-3 me-1 border-0'
                 aria-label="Scroll to top"
             >
