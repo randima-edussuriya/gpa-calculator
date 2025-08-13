9250295d9b97aa11369dbd22ea2abda8f2542a6b
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-08-13 19:17:23 +0530

    refactor: remove empty lines

--- src/components/ModuleForm.jsx
+++ src/components/ModuleForm.jsx
@@ -3,58 +3,57 @@ import { Form, Row, Col, Button } from 'react-bootstrap'
 
 function ModuleForm({ formData, gradingSystem, handleSubmit, handleChange }) {
     return (
-        <>
-            <Form onSubmit={handleSubmit}>
-                <Row className="align-items-center g-2 justify-content-center">
-                    <Col md='6'>
-                        <Form.Label htmlFor="moduleTitle" visuallyHidden>
-                            Module Title
-                        </Form.Label>
-                        <Form.Control
-                            id="moduleTitle"
-                            type='text'
-                            name='moduleTitle'
-                            placeholder="Module Title"
-                            value={formData.moduleTitle}
-                            onChange={handleChange}
+        <Form onSubmit={handleSubmit}>
+            <Row className="align-items-center g-2 justify-content-center">
+                <Col md='6'>
+                    <Form.Label htmlFor="moduleTitle" visuallyHidden>
+                        Module Title
+                    </Form.Label>
+                    <Form.Control
+                        id="moduleTitle"
+                        type='text'
+                        name='moduleTitle'
+                        placeholder="Module Title"
+                        value={formData.moduleTitle}
+                        onChange={handleChange}
 
-                        />
-                    </Col>
-                    <Col md='auto'>
-                        <Form.Label htmlFor="credit" visuallyHidden>
-                            Credit
-                        </Form.Label>
-                        <Form.Control
-                            id="credit"
-                            type='number'
-                            name='credit'
-                            placeholder="Credit"
-                            min='0'
-                            value={formData.credit}
-                            onChange={handleChange}
+                    />
+                </Col>
+                <Col md='auto'>
+                    <Form.Label htmlFor="credit" visuallyHidden>
+                        Credit
+                    </Form.Label>
+                    <Form.Control
+                        id="credit"
+                        type='number'
+                        name='credit'
+                        placeholder="Credit"
+                        min='0'
+                        value={formData.credit}
+                        onChange={handleChange}
 
-                        />
-                    </Col>
-                    <Col md="auto">
-                        <Form.Label htmlFor='grade' visuallyHidden>
-                            Grade
-                        </Form.Label>
-                        <Form.Select id='grade' name='grade' value={formData.grade} onChange={handleChange} >
-                            <option value=''>Select Grade</option>
-                            {
-                                gradingSystem.map((row, index) => (
-                                    <option value={row.grade} key={index}>{row.grade}</option>
-                                ))
-                            }
-                        </Form.Select>
-                    </Col>
-                    <Col md="auto">
-                        <Button variant='dark' type="submit">
-                            Add Module
-                        </Button>
-                    </Col>
-                </Row>
-            </Form></>
+                    />
+                </Col>
+                <Col md="auto">
+                    <Form.Label htmlFor='grade' visuallyHidden>
+                        Grade
+                    </Form.Label>
+                    <Form.Select id='grade' name='grade' value={formData.grade} onChange={handleChange} >
+                        <option value=''>Select Grade</option>
+                        {
+                            gradingSystem.map((row, index) => (
+                                <option value={row.grade} key={index}>{row.grade}</option>
+                            ))
+                        }
+                    </Form.Select>
+                </Col>
+                <Col md="auto">
+                    <Button variant='dark' type="submit">
+                        Add Module
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
     )
 }
 
--- src/components/Navigations/NavBar.jsx
+++ src/components/Navigations/NavBar.jsx
@@ -4,19 +4,17 @@ import DarkModeToggle from './DarkModeToggle'
 
 function NavBar({ handleShow }) {
     return (
-        <>
-            <Navbar className='navBar bg-dark-subtle sticky-top shadow'>
-                <Container fluid className=''>
-                    <Navbar.Brand >
-                        <span className='h3'>GPA Calculator</span>
-                    </Navbar.Brand>
-                    <Nav className="ms-auto gap-3">
-                        <DarkModeToggle />
-                        <i className="navIcon bi bi-question-circle fs-2" onClick={handleShow}></i>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
+        <Navbar className='navBar bg-dark-subtle sticky-top shadow'>
+            <Container fluid className=''>
+                <Navbar.Brand >
+                    <span className='h3'>GPA Calculator</span>
+                </Navbar.Brand>
+                <Nav className="ms-auto gap-3">
+                    <DarkModeToggle />
+                    <i className="navIcon bi bi-question-circle fs-2" onClick={handleShow}></i>
+                </Nav>
+            </Container>
+        </Navbar>
     )
 }
 
